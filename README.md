60622d3a4d5d0e1f3ef98d8ba5b30637a8e9f205
aydn-burner <hooperaydn@gmail.com>
2022-05-13 14:44:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-dp6rnt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-dp6rnt)
---
+++ index.ts
@@ -0,0 +1,13 @@
+// write a function that will draw a square in the console
+//sample output:
+// ####
+// ####
+// ####
+// ####
+
+//write a function that will draw a triangle in the console
+//sample output:
+//  *
+//  * *
+//  * * *
+//  * * * *
